28ccc2a81dba1823b1174641b8af42bc916a9249
Mike Shilovski <ShMike6491@gmail.com>
2021-02-07 14:16:32 +0300

    model corrections

---
+++ app/src/main/java/geekbrains/android/CalculatorService.java
@@ -0,0 +1,10 @@
+package geekbrains.android;
+
+public interface CalculatorService {
+    double getScreenSaver();
+    void setScreenSaver (String num);
+
+    double operate(String number, char operator);
+
+    boolean errorPrevent (String num);
+}
